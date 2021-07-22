f95c058ada042385e5d27e7b0a63c91a23d74687
DuongBaNhuan2020 <73644949+DuongBaNhuan2020@users.noreply.github.com>
2021-07-22 20:35:01 +0700

    them ts

--- TheGioiDoChoi/src/app/blog-detail/blog-detail.component.ts
+++ TheGioiDoChoi/src/app/blog-detail/blog-detail.component.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-
+import {Component, Input, OnInit} from '@angular/core';
+import {Blog} from "../model/blog";
 @Component({
   selector: 'app-blog-detail',
   templateUrl: './blog-detail.component.html',
   styleUrls: ['./blog-detail.component.scss']
 })
 export class BlogDetailComponent implements OnInit {
+  public blog:Blog={
+    name: "Ten blog",
+    date: "22/07/2021",
+    author: "Nam",
+    content: "Day la content",
+    img1: "Day la link img"
+  }
 
   constructor() { }
-
   ngOnInit(): void {
   }
 

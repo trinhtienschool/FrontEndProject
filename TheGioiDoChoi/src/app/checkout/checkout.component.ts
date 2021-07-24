cd608524ddb1174f7696e5e0a568a60852f6e0ac
DuongBaNhuan2020 <73644949+DuongBaNhuan2020@users.noreply.github.com>
2021-07-24 14:00:44 +0700

    update ts for header

--- TheGioiDoChoi/src/app/checkout/checkout.component.ts
+++ TheGioiDoChoi/src/app/checkout/checkout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CartItem} from "../model/cart-item";
 
 @Component({
   selector: 'app-checkout',
@@ -6,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.scss']
 })
 export class CheckoutComponent implements OnInit {
+  public cartItem: CartItem[]=[
+    new CartItem("Do choi1","Img1",2000,2),
+    new CartItem("Do choi2","Img2",2000,3),
+    new CartItem("Do choi3","Img3",2000,5)
+  ]
 
   constructor() { }
+  public  totalPriceOfAllProduct(){
+    let sum =0
+    for(let c of this.cartItem){
+      sum+=c.totalPrice();
+    }
+    return sum;
+  }
+  public numberOfListProduct(){
+    let number=0;
+    for(let c of this.cartItem){
+      number+=1;
+    }
+    return number;
+  }
+  public deleteAcartItem(){
 
+  }
   ngOnInit(): void {
   }
 

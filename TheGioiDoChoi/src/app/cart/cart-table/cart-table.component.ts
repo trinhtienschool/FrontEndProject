783846278bd81ae1519e3d9261674c696c792bc8
Trần Nhật Thy <73516408+nhatthy24@users.noreply.github.com>
2021-07-25 20:22:55 +0700

    Tao component cho trang gio hang, chi tiet sản phẩm, footer

--- TheGioiDoChoi/src/app/cart/cart-table/cart-table.component.ts
+++ TheGioiDoChoi/src/app/cart/cart-table/cart-table.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {CartItem} from "../../model/cart-item";
+import {CartService} from "../../service/messenger/cart.service";
+import {Product} from "../../model/product";
 
 @Component({
   selector: 'app-cart-table',
@@ -7,13 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartTableComponent implements OnInit {
   // public products: Product[] =[];
-  cartItems = []
+  cartItems : CartItem[] = []
 
-  constructor() {
-
+  constructor(private cartService: CartService) {
+    this.cartService.cart$.subscribe(cart =>{this.cartItems = cart});
   }
 
   ngOnInit(): void {
+
   }
 
+  // todo nút giảm số lượng sản phẩm
+  decreaseQuantity(item:CartItem) {
+    if(item.quantity>1)
+      item.quantity -= 1;
+    else if(item.quantity==1)
+      this.cartService.deleteItemProduct(item)
+  }
+  // todo nút tăng số lượng sản phẩm
+  increaseQuantity(item:CartItem) {
+    item.quantity += 1;
+  }
+
+
+  deleteItem(item: CartItem) {
+   this.cartService.deleteItemProduct(item)
+    // alert("da vao xóa " + item)
+  }
 }

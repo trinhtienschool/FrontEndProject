40ee20239f20318acf0fa94543dae4fa77504605
trinhtienschool <18130243@st.hcmuaf.edu.vn>
2021-09-01 21:17:21 +0700

    Lam xong dieu huong trang chu

--- TheGioiDoChoi/src/app/home/age-area/age-area.component.ts
+++ TheGioiDoChoi/src/app/home/age-area/age-area.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-age-area',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgeAreaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
-
+  age_click(text: string){
+    this.router.navigateByUrl("/product?age="+text);
+  }
 }

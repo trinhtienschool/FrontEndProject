d08410ca75398a90bb268186154a21209f49b8c0
DuongBaNhuan2020 <73644949+DuongBaNhuan2020@users.noreply.github.com>
2021-09-15 19:37:58 +0700

    add love for card sale

--- TheGioiDoChoi/src/app/share/product-card-sale/product-card-sale.component.ts
+++ TheGioiDoChoi/src/app/share/product-card-sale/product-card-sale.component.ts
@@ -2,6 +2,7 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Product} from "../../model/product";
 import {CartService} from "../../service/cart/cart.service";
 import {fade} from "../animation";
+import {LoveService} from "../../service/love/love.service";
 // declare const clickAddCart: any;
 @Component({
   selector: 'app-product-card-sale',
@@ -13,12 +14,16 @@ import {fade} from "../animation";
 })
 export class ProductCardSaleComponent implements OnInit {
  @Input() product: Product|undefined;
+  @Input() productLove: Product|undefined;
  @Output() getProductQuickView: EventEmitter<Product> = new EventEmitter<Product>();
  showCart = true;
   showQuickView = false;
  changeImage = false;
 
-  constructor(private cartService: CartService) {
+  constructor(private cartService: CartService,private loveService: LoveService) {
+    this.loveService.love$.subscribe(love => {
+      this.productLove = love
+    })
   }
 
   ngOnInit(): void {
@@ -38,7 +43,8 @@ export class ProductCardSaleComponent implements OnInit {
     this.getProductQuickView.emit(this.product);
   }
 
-  onClickLoveProduct() {
+  onClickLoveProduct(product: Product) {
     //Nhuan does here
+    this.loveService.addLove(product);
   }
 }

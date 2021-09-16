3f852318ec3f2a08698008fbd47fa1c4f123a241
trinhtienschool <18130243@st.hcmuaf.edu.vn>
2021-09-16 20:17:20 +0700

    Sua add love o trang chu

--- TheGioiDoChoi/src/app/app.component.ts
+++ TheGioiDoChoi/src/app/app.component.ts
@@ -50,7 +50,7 @@ export class AppComponent implements OnInit{
   @HostListener('window:scroll')
   checkScroll() {
     const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    console.log('[scroll]', scrollPosition);
+    // console.log('[scroll]', scrollPosition);
     if (scrollPosition >= this.topPosToStartShowing) {
       this.isShow = true;
     } else {
--- TheGioiDoChoi/src/app/share/product-card-sale/product-card-sale.component.ts
+++ TheGioiDoChoi/src/app/share/product-card-sale/product-card-sale.component.ts
@@ -3,6 +3,7 @@ import {Product} from "../../model/product";
 import {CartService} from "../../service/cart/cart.service";
 import {fade} from "../animation";
 import {LoveService} from "../../service/love/love.service";
+
 // declare const clickAddCart: any;
 @Component({
   selector: 'app-product-card-sale',
@@ -13,38 +14,60 @@ import {LoveService} from "../../service/love/love.service";
   ]
 })
 export class ProductCardSaleComponent implements OnInit {
- @Input() product: Product|undefined;
-  @Input() productLove: Product|undefined;
- @Output() getProductQuickView: EventEmitter<Product> = new EventEmitter<Product>();
- showCart = true;
-  showQuickView = false;
- changeImage = false;
+  @Input() product: Product | undefined;
 
-  constructor(private cartService: CartService,private loveService: LoveService) {
-    this.loveService.love$.subscribe(love => {
-      this.productLove = love
-    })
+  @Output() getProductQuickView: EventEmitter<Product> = new EventEmitter<Product>();
+  showCart = true;
+  showQuickView = false;
+  changeImage = false;
+  isLove = false;
+
+
+  constructor(private cartService: CartService, private loveService: LoveService) {
+    // this.loveService.love$.subscribe(love => {
+    //   this.productLove = love
+    //   if(this.productLove !=undefined && this.product !=undefined){
+    //     for(let i = 0;i<this.productLove.length;i++){
+    //       if(this.productLove[i].product_id==this.product.product_id){
+    //         this.isLove = true;
+    //         console.log('id product card sale: ',this.product.product_id);
+    //       }
+    //     }
+    //   }
+    // })
+
   }
 
   ngOnInit(): void {
-
+    let productLoves = this.loveService.love;
+    if (this.product != undefined) {
+      for (let i = 0; i < productLoves.length; i++) {
+        if (this.product.product_id == productLoves[i].product_id) {
+          this.isLove = true;
+        }
+      }
+    }
   }
-  handleAddToCart(){
-    if(this.product != undefined){
-      this.cartService.addCart(this.product,1);
-      this.showCart= false;
-      setTimeout(()=>this.showCart=true,1000);
+
+  handleAddToCart() {
+    if (this.product != undefined) {
+      this.cartService.addCart(this.product, 1);
+      this.showCart = false;
+      setTimeout(() => this.showCart = true, 1000);
 
     }
   }
-  onClickProductQuickView(){
-    console.log("co vao productcardsaleeeeeeeeee: ",this.product);
-    if(this.product !=undefined)
-    this.getProductQuickView.emit(this.product);
+
+  onClickProductQuickView() {
+    console.log("co vao productcardsaleeeeeeeeee: ", this.product);
+    if (this.product != undefined)
+      this.getProductQuickView.emit(this.product);
   }
 
   onClickLoveProduct(product: Product) {
     //Nhuan does here
     this.loveService.addLove(product);
+    this.isLove = true;
+    console.log('love', product);
   }
 }

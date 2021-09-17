2e541c7d20ff0fe8ac7a9eb4b16e107412e8ce70
DuongBaNhuan2020 <73644949+DuongBaNhuan2020@users.noreply.github.com>
2021-09-17 15:14:29 +0700

    add loveable page

--- TheGioiDoChoi/src/app/love/loveable/loveable.component.ts
+++ TheGioiDoChoi/src/app/love/loveable/loveable.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {Product} from "../../model/product";
 import {ProductService} from "../../service/product/product.service";
 import {CartService} from "../../service/cart/cart.service";
@@ -8,8 +8,8 @@ import {CartService} from "../../service/cart/cart.service";
   templateUrl: './loveable.component.html',
   styleUrls: ['./loveable.component.scss']
 })
-export class LoveableComponent implements OnInit {
-  public listProducts: Product[] =[];
+export class LoveableComponent implements OnInit,AfterViewInit {
+  public listProducts: Product[]|undefined;
   productQuickView: Product | undefined
   constructor(private productService: ProductService,
               private cartService: CartService) {
@@ -26,5 +26,12 @@ export class LoveableComponent implements OnInit {
   getQuickView($event: Product) {
     this.productQuickView = $event
   }
-}
 
+  ngAfterViewInit(): void {
+    setTimeout(()=>{
+      console.log('random products: ',this.productService.getRandomProducts());
+      this.listProducts = this.productService.getRandomProducts();
+    },500)
+
+  }
+}
